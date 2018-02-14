a74916b52bb4644a1b3f41f13e2e86982ccc09ab
danlizard <30766104+danlizard@users.noreply.github.com>
2018-02-14 18:14:35 +0300

    oh, these bug(ger)s

--- handler.py
+++ handler.py
@@ -1,6 +1,6 @@
 from datetime import *
 def handle_message(message, nickname="user"):
-	mtxt = message
+	mtxt = message.split()
 	answer = ''
 	t = 0
 	userlist = open("users.txt", 'r')
@@ -10,39 +10,44 @@ def handle_message(message, nickname="user"):
 	grades = grd.readlines()
 	grd.close()
 	Week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+	print(nickname, uscheck)
 	if nickname not in uscheck:
-		if 3 > len(message) > 0 and "9" >= message[0] >= "0":
-			usernew = open("users.txt", 'w')
-			print(nickname, file=usernew)
-			usernew.close()
-			gradenew = open("grades.txt", 'w')
-			print(mtxt, file=gradenew)
-			gradenew.close()
+		if 3 > len(message) > 0 and "9" >= message >= "0":
+			with open("users.txt", 'a') as usernew:
+				print(nickname, file=usernew)		
+			with open("grades.txt", 'a') as gradenew:
+				print("\n".join(*mtxt), file=gradenew)
+			
 		else:
 			answer = 'Please input your grade number.'
 	else:
-		usnumber = uscheck.find(nickname)
-		grade = grades[usnumber]
-		if mtxt.lower in Week:
-			day = mtxt.lower
+		for i in range(0, len(uscheck)):
+			if nickname.rstrip() == uscheck[i].rstrip():
+				usnumber = i
+				break
+		grade = grades[usnumber].rstrip()
+		if mtxt[0].rstrip().lower() in Week:
+			day = mtxt[0].rstrip().lower()
 		else:
 			day = date.today().weekday()
 			day = Week[day]
 		path = grade + '/' + day + '.txt'
-		schedule = open(path, 'w')
+		schedule = open(path, 'r')
 		answer = schedule.readlines()
-		if answer = '':
-			answer = "You either have no lessons tomorrow or we don't have information about them."
+		if len(answer) == 0:
+			answer = "You either have no lessons today or we don't have information about them."
+		else:
+			print("40", answer)
 		schedule.close()
 
-    return answer
+	return answer
 
 
 if __name__ == "__main__":
-    nick = input("Enter your nickname: ")
+	nick = input("Enter your nickname: ")
 
-    while True:
-        msg = input("Your message: ")
-        ans = handle_message(msg, nick)
+	while True:
+		msg = input("Your message: ")
+		ans = handle_message(msg, nick)
 
-        print(ans)
+	print(ans)
--- system.py
+++ system.py
@@ -1,15 +1,19 @@
 import requests 
-from handler.py import handle_message 
+from handler import handle_message 
 token = "https://api.telegram.org/bot540889680:AAHVdAwR1JxSfJWP61-i0g1eVK-LoK9QfOU/"
-
+last_update = 584957547
 while True:
-	req = requests.get(token + "getUpdates")
+	temp = dict()
+	temp["offset"] = last_update
+	req = requests.get(token + "getUpdates", params=temp)
 	Inp = req.json()
 
 	for el in Inp["result"]:
+		last_update = max(el["update_id"], last_update)
+		last_update += 1
 		text = el["message"]["text"]
-		nick = el["message"]["from"]["username"]
+		nick = str(el["message"]["from"]["id"]) + "\n"
 		chat_id = el["message"]["from"]["id"]
 		ans = handle_message(text, nick)
-
-		requests.post(token + "sendMessage", params=("chat_id": chat_id, "text": text))
+		for i in range(0, len(ans)):
+			requests.post(token + "sendMessage", params={"chat_id": chat_id, "text": ans[i]})
